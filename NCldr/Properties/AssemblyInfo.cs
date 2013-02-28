cfb63963b86101e2d7850006729f035e84f95705
Guy Smith-Ferrier <guy@guysmithferrier.com>
2013-02-28 11:49:32 +0000

    Changed the AssemblyInfo information in NCldr and NCldrExtensions

--- NCldr/Properties/AssemblyInfo.cs
+++ NCldr/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NCldr")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("NCldr is an open source .NET port of CLDR")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Guy Smith-Ferrier")]
 [assembly: AssemblyProduct("NCldr")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.001")]
+[assembly: AssemblyFileVersion("0.0.0.001-alpha")]
+[assembly: AssemblyInformationalVersion("0.0.0.001-alpha")]
--- NCldrExtensions/Properties/AssemblyInfo.cs
+++ NCldrExtensions/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NCldrExtensions")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("NCldr Extensions")]
+[assembly: AssemblyDescription("NCldr Extensions is a set of extensions for NCLDR and .NET's CultureInfo and RegionInfo classes")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Guy Smith-Ferrier")]
 [assembly: AssemblyProduct("NCldrExtensions")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.001")]
+[assembly: AssemblyFileVersion("0.0.0.001-alpha")]
+[assembly: AssemblyInformationalVersion("0.0.0.001-alpha")]
