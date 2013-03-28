03966d909729b90f8a268f0198f5e758981ea278
Guy Smith-Ferrier <guy@guysmithferrier.com>
2013-03-28 10:11:28 +0000

    Renamed the NCldrDataSource.cs file
    Renamed the NCldrDataSource.cs file to NCldrBinaryFileDataSource.cs

--- NCldr/NCldrLoader.cs
+++ NCldr/NCldrLoader.cs
@@ -6,7 +6,7 @@
     /// NCldrLoader loads/saves the raw NCLDR data from an NCldr data file
     /// </summary>
     /// <remarks>Set the NCldrLoader.NCldrDataPath property before calling NCldrLoader.Load</remarks>
-    [Obsolete("NCldrLoader is deprecated. Use NCldrDataSource instead.")]
+    [Obsolete("NCldrLoader is deprecated. Use NCldrBinaryFileDataSource instead.")]
     public class NCldrLoader : NCldrBinaryFileDataSource
     {
     }
