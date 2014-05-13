a3bdca6f00f899fe072c424952b3b1a0a31fed96
Guy Smith-Ferrier <guy@guysmithferrier.com>
2014-05-13 11:54:31 +0100

    Updated version numbers

--- NCldrExtensions/Properties/AssemblyInfo.cs
+++ NCldrExtensions/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.0.007")]
-[assembly: AssemblyFileVersion("0.0.0.007")]
-[assembly: AssemblyInformationalVersion("0.0.0.007")]
+[assembly: AssemblyVersion("0.0.0.008")]
+[assembly: AssemblyFileVersion("0.0.0.008")]
+[assembly: AssemblyInformationalVersion("0.0.0.008")]
