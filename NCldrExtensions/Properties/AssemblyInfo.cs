7658b348f0017f318b692af603b13975296fcf0a
Guy Smith-Ferrier <guy@guysmithferrier.com>
2014-06-19 09:32:20 +0100

    Updated version numbers

--- NCldrExtensions/Properties/AssemblyInfo.cs
+++ NCldrExtensions/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Guy Smith-Ferrier")]
 [assembly: AssemblyProduct("NCldrExtensions")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.0.008")]
-[assembly: AssemblyFileVersion("0.0.0.008")]
-[assembly: AssemblyInformationalVersion("0.0.0.008")]
+[assembly: AssemblyVersion("0.0.0.009")]
+[assembly: AssemblyFileVersion("0.0.0.009")]
+[assembly: AssemblyInformationalVersion("0.0.0.009")]
--- NCldrThirdPartyDataSources/Properties/AssemblyInfo.cs
+++ NCldrThirdPartyDataSources/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NCldrThirdPartyDataSources")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("NCldr Third Party Data Sources")]
+[assembly: AssemblyDescription("A collection of NCLDR Data Sources that use third party libraries")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Guy Smith-Ferrier")]
 [assembly: AssemblyProduct("NCldrThirdPartyDataSources")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.009")]
+[assembly: AssemblyFileVersion("0.0.0.009")]
+[assembly: AssemblyInformationalVersion("0.0.0.009")]
